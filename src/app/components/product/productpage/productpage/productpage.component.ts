92d0a8ac838a3fd2eee08488410fbe9192f4a4b4
wil <nimrodcorymbe@gmail.com>
2022-07-02 03:22:09 +0200

    finish productpage

--- src/app/components/product/productpage/productpage/productpage.component.ts
+++ src/app/components/product/productpage/productpage/productpage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Bob } from 'src/app/models/product/bobClass';
 import { BobService } from 'src/app/service/bobService/bob.service';
 
@@ -15,20 +15,50 @@ export class ProductpageComponent implements OnInit {
   bobSizeLenght ?: number ; 
   sizeMessage ?: string ;
 
+  colorLenght ?: number ;
+  colorMessage ?: string ;
+
+  sameBobArray ?: Bob[] ;
+
+  samebobArrayLenght ?: number ;
+
+  index = 0 ;
+
   constructor(
     private ActivatedRoute : ActivatedRoute,
-    private bobService : BobService
+    private bobService : BobService,
+    private router : Router
   ) { }
 
   ngOnInit(): void {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+
     this.id = parseInt(<string>this.ActivatedRoute.snapshot.paramMap.get('id')) ;
 
     this.bobDisplayer = this.bobService.getBobById(this.id)[0] ;
 
     this.bobSizeLenght = this.bobDisplayer.size.length;
     this.sizeMessage = this.bobService.displaySizeMessage(this.bobSizeLenght);
-    console.log(this.bobDisplayer);
+
+    this.colorLenght = this.bobDisplayer.color.length ;
+    this.colorMessage = this.bobService.displayColorMessage(this.colorLenght) ;
+
+    this.sameBobArray = this.bobService.getBobByCategWithoutOne(this.bobDisplayer.category, this.id);
+
+    this.samebobArrayLenght = this.sameBobArray.length ;
   }
 
 
+  nextElementCarousel() {
+    if (this.samebobArrayLenght) {
+      if (this.index == this.samebobArrayLenght-1) {    
+      } else this.index++ ;
+    }
+  }
+
+  prevElementCarousel() {
+    if (this.index > 0) {
+      this.index--;
+    }
+  }
 }
