82cc7bee3965cbc87d31596bf7f0f6cfa297ec13
wil <nimrodcorymbe@gmail.com>
2022-07-08 22:00:29 +0200

    gaurds admin + user

---
+++ src/app/components/guards/adminGuard/admin-guard.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthServiceService } from 'src/app/service/auth/auth-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuardGuard implements CanActivate {
+  constructor(
+    private authService : AuthServiceService,
+    private router : Router
+  ) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if (this.authService.isLoggedIn !== true) {
+        this.router.navigate(['/noAcess']);
+      } else if (this.authService.getToken() !== 'adminadmin' ) {
+        this.router.navigate(['/noAcess']);
+      }
+    return true;
+  }
+  
+}
